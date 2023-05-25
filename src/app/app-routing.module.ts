84b97de7e453fdda81bbb6b3ca1bde3b9ff19b83
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-25 23:58:34 +0530

    Added admin dashboard component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { MyOrdersComponent } from './product/my-orders/my-orders.component';
+import { ProductsComponent } from './product/products/products.component';
+import { CartComponent } from './product/cart/cart.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: 'my-orders', component: MyOrdersComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
