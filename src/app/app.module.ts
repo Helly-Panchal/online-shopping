cc47f47ba3b9771a25a1cf4c3afa420c1cb09de8
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-25 14:58:49 +0530

    Firebase Configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment.development';
+
+// components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './main/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCyUXHQU6zFXag-z_wb4_cyOhMwqItHuxk",
+    authDomain: "online-shopping-c916e.firebaseapp.com",
+    databaseURL: "https://online-shopping-c916e-default-rtdb.firebaseio.com",
+    projectId: "online-shopping-c916e",
+    storageBucket: "online-shopping-c916e.appspot.com",
+    messagingSenderId: "481766238051",
+    appId: "1:481766238051:web:e4bfe4b72de68b31ced556"
+  }
+};
