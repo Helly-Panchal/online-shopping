0278c711126ec3b83293461edf0fcfd689704a74
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-25 23:11:16 +0530

    Created my-orders component

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart/cart.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 
 
 @NgModule({
   declarations: [
-    CartComponent
+    CartComponent,
+    MyOrdersComponent
   ],
   imports: [
     CommonModule
