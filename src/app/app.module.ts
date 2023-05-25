5644f449e0efd72f765394b1ba8bb7e9de155629
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-25 16:06:06 +0530

    Added auth module which contain login and register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment.development';
+import { AuthModule } from './auth/auth.module';
 
 // components
 import { AppComponent } from './app.component';
-import { LoginComponent } from './main/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
