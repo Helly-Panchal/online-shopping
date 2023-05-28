97bf95796acde5b41418195bb97fd0a72566528b
Helly-Panchal <helly.panchal@bacancy.com>
2023-05-28 15:46:25 +0530

    Created products component in admin module

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,
