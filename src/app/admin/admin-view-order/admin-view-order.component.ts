692addda2053f15bac9212a5d1bd9e28be28441e
Helly-Panchal <helly.panchal@bacancy.com>
2023-06-02 14:15:53 +0530

    Completed view order at admin side

--- src/app/admin/admin-view-order/admin-view-order.component.ts
+++ src/app/admin/admin-view-order/admin-view-order.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { IOrder } from 'src/app/interfaces/order.interface';
+import { IProduct } from 'src/app/interfaces/product.interface';
+import { PlaceOrderService } from 'src/app/services/place-order.service';
 
 @Component({
   selector: 'app-admin-view-order',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-view-order.component.scss']
 })
 export class AdminViewOrderComponent {
+  public item!: IProduct[];
 
+  constructor(@Inject(MAT_DIALOG_DATA) public data: IOrder, public dialogRef: MatDialogRef<AdminViewOrderComponent>,
+    private placeOrderService: PlaceOrderService) {
+    this.item = data.item;
+    console.log(data);
+  }
+
+  public ngOnInit(): void {
+    this.placeOrderService.getOrderedItems();
+  }
+
+  public close() {
+    this.dialogRef.close();
+  }
 }
