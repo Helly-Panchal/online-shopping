aceba4fba61c8d063bfc802e3711b07766c98ff8
Helly-Panchal <helly.panchal@bacancy.com>
2023-06-09 15:07:59 +0530

    Created test cases for register component

--- src/app/auth/register/register.component.spec.ts
+++ src/app/auth/register/register.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,9 +11,9 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      declarations: [RegisterComponent],
+      imports: [AngularFireModule.initializeApp(environment.firebase), FormsModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
@@ -19,5 +22,9 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('initial value of isError is false', () => {
+    expect(component.isError).toEqual(false);
   });
 });
