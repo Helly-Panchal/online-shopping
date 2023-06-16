fe97abb56d873bb948df3e586ba1f209e6356828
Helly-Panchal <helly.panchal@bacancy.com>
2023-06-16 10:29:17 +0530

    Created auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModuleModule } from '../shared-module/shared-module.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { FormsModule } from '@angular/forms'
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent, PageNotFoundComponent],
+  imports: [
+    CommonModule,
+    SharedModuleModule,
+    FormsModule
+  ],
+  exports: [LoginComponent, RegisterComponent, PageNotFoundComponent]
+})
+export class AuthModule { }
